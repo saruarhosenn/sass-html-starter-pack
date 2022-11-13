6ddedda4227cebe69cf1e1840306c0533e8612ee
Saruar Hosen <77750320+saruarhosenn@users.noreply.github.com>
2022-11-13 19:34:51 +0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,35 @@ window.addEventListener("load", vanish);
 function vanish() {
   loader.classList.add("disppear");
 }
+
+/* ===== NavBar Open & Close Humburger Button ===== */
+      function myFunction(x) {
+        x.classList.toggle("change");
+      }
+
+      /* ===== Scroll Back To Top Button ===== */
+      var mybutton = document.getElementById("scroll-up-btn");
+      window.onscroll = function () {
+        scrollFunction();
+      };
+
+      function scrollFunction() {
+        if (
+          document.body.scrollTop > 50 ||
+          document.documentElement.scrollTop > 50
+        ) {
+          mybutton.style.opacity = "1";
+        } else {
+          mybutton.style.opacity = "0";
+        }
+      }
+      mybutton.addEventListener("click", topFunction);
+
+      function topFunction() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+      }
+
+      /* ======== Mixitup ======== */
+      var containerEl = document.querySelector(".container");
+      var mixer = mixitup(containerEl);

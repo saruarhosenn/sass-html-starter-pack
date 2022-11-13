f3bc24289bcf4d4c30a20fdc340685e92b40c1fe
Saruar Hosen <77750320+saruarhosenn@users.noreply.github.com>
2022-11-13 19:31:43 +0600

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,3 @@
-
-/*---------------------------------------------
-Template name: .
-Version: 1.0.0
-
-[Table of Content]
-----------------------
-01: Preloader
-02: Mixit UP
-03: Lity
-04: Swiper Slider
-05: Back to Top
-06: back to top button animate
-----------------------------------------------*/
-
 (function($) {
     "use strict";
 
